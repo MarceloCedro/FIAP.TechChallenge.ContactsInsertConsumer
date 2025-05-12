db2fd210b86716a21cf5bb8ca500d9b9f69831c1
rodrigo mahlow <rmahlow@gmail.com>
2025-05-12 16:27:54 -0300

    add services

--- ContactsInsertConsumer.Api/Kubernets/deploy.sh
+++ ContactsInsertConsumer.Api/Kubernets/deploy.sh
@@ -11,6 +11,9 @@ kubectl apply -f Configmap.yaml  # Aplica o ConfigMap
 echo "📦 Aplicando Deployment da aplicação..."
 kubectl apply -f Deployment.yaml  # Aplica o Deployment da aplicação
 
+echo "🚪 Aplicando Service da aplicação..."
+kubectl apply -f Service.yaml  # Aplica o Service da aplicação
+
 echo "📈 Aplicando Horizontal Pod Autoscaler (HPA)..."
 kubectl apply -f hpa.yaml  # Aplica o HPA a partir do arquivo hpa.yam
 
